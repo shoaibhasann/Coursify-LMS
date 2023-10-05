21989ee729ac6e8ab1be3f4fefe42bfbed316bf2
shoaibhasann <shoaibhasan0940@gmail.com>
2023-10-05 20:52:41 +0530

    add .env example file

---
+++ server/.env.example.js
@@ -0,0 +1,14 @@
+PORT=8000
+MONGO_URI=mongodb+srv://your_username:your_password@cluster0.9vsodbx.mongodb.net/LMS
+NODE_ENV=Your_Node_Enviroment
+
+JWT_SECRET=Your_JWT_Secret
+JWT_EXPIRY=24h
+
+CLOUDINARY_CLOUD_NAME=Your_Cloudinary_Cloud_Name
+CLOUDINARY_API_KEY=Your_Cloudinary_API_Key
+CLOUDINARY_API_SECRET=Your_Cloudinary_API_Secret
+
+RAZORPAY_KEY_ID=Your_Razorpay_Key_ID
+RAZORPAY_SECRET=Your_Razorpay_Secret
+RAZORPAY_PLAN_ID=Your_Razorpay_Plan_ID
