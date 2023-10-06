3cf991be7e83c7e81ba32f1975cc0bbda441c11a
shoaibhasann <shoaibhasan0940@gmail.com>
2023-10-06 16:40:39 +0530

    set up all routes for user

--- server/route/user.route.js
+++ server/route/user.route.js
@@ -1,19 +1,26 @@
 import { Router } from "express";
-import { changePassword, editProfile, forgotPassword, getProfile, login, logout, register, resetPassword } from "../controller/user.controller.js";
+import {
+  changePassword,
+  editProfile,
+  forgotPassword,
+  getProfile,
+  login,
+  logout,
+  register,
+  resetPassword,
+} from "../controller/user.controller.js";
 import { isLoggedIn } from "../middleware/auth.middleware.js";
 import upload from "../middleware/multer.middleware.js";
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
-router.post('/forgot-password', forgotPassword);
-router.post('/reset-password/:resetToken', resetPassword);
-router.post('/change-password', isLoggedIn, changePassword);
-router.put("/update", isLoggedIn,  upload.single("avatar"), editProfile);
-router.get('/logout', logout);
-router.get('/me', isLoggedIn, getProfile);
-
-
+router.post("/register", register);
+router.post("/login", login);
+router.post("/forgot-password", forgotPassword);
+router.post("/reset-password/:resetToken", resetPassword);
+router.post("/change-password", isLoggedIn, changePassword);
+router.put("/update", isLoggedIn, upload.single("avatar"), editProfile);
+router.get("/logout", logout);
+router.get("/me", isLoggedIn, getProfile);
 
 export default router;
