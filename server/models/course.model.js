29bc9fa985532345255d4b4cbd514c474c203fe2
shoaibhasann <shoaibhasan0940@gmail.com>
2023-10-07 16:38:38 +0530

    create course schema

---
+++ server/models/course.model.js
@@ -0,0 +1,93 @@
+import { Schema, model } from "mongoose";
+
+const courseSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      public_id: {
+        type: String
+      },
+      secure_url: {
+        type: String
+      },
+    },
+    instructors: [
+      {
+          type: String,
+          required: true
+      },
+    ],
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
+    discount: {
+      type: Number,
+      default: 0
+    },
+    duration: {
+      type: String,
+      required: true
+    },
+    language: {
+      type: String,
+      required: true
+    },
+    salary: {
+      type: String,
+    },
+    roles: {
+      type: String,
+    },
+    skills: {
+      type: String,
+      required: true
+    },
+    eligibility: {
+      type: String,
+      required: true
+    },
+    lectures: [
+      {
+        title: String,
+        description: String,
+        lectureThumbnail: {
+          public_id: {
+            type: String,
+          },
+          secure_url: {
+            type: String,
+          },
+        },
+      },
+    ],
+    numberOfLectures: {
+      type: Number,
+      default: 0
+    },
+    createdBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User"
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Course = model("Course", courseSchema);
+
+export default Course;
