9a55f6eadd5e834a6fbe16ffb1628c82ad82bd8f
shoaibhasann <shoaibhasan0940@gmail.com>
2023-10-10 19:39:13 +0530

    Added Not found page component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,12 +3,15 @@ import { Route, Routes } from 'react-router-dom'
 
 import HomePage from './pages/HomePage.jsx'
 import AboutUs from './pages/AboutUs.jsx'
+import NotFound from './pages/NotFound.jsx'
 
 function App() {
   return (
     <Routes>
       <Route path='/' element={<HomePage/>}/>
       <Route path='/about' element={<AboutUs/>}/>
+
+      <Route path='*' element={<NotFound/>}/>
     </Routes>
   )
 }
