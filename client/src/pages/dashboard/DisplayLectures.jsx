dea3486b156fdb65f960e30b91c03ee50d910d38
shoaibhasann <shoaibhasan0940@gmail.com>
2023-10-18 13:16:02 +0530

    Added watching lecture functionality

--- client/src/pages/dashboard/DisplayLectures.jsx
+++ client/src/pages/dashboard/DisplayLectures.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from "react";
-import { AiOutlineArrowLeft } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux"
 import { useLocation, useNavigate } from "react-router-dom";
 
@@ -28,86 +27,97 @@ function DisplayLectures() {
      }
     }
 
+    let vid = document.getElementById("video");
+    console.log(vid);
+    
+
   return (
     <HomeLayout>
-      <div className="flex flex-col gap-10 items-center justify-center min-h-[100vh] pt-15 py-10 text-white">
+      <div className="flex flex-col gap-10 items-center justify-center min-h-[100vh] py-16 sm:py-10 text-wihte mx-[5%]">
+        <div className="text-center text-2xl font-semibold text-yellow-500">
+          {lectures && lectures.length > 0
+            ? (state?.title)
+            : "Sorry this course doesn't have lectures yet."}
+        </div>
+
         {lectures && lectures.length > 0 ? (
-          <>
-            <div className="text-center text-2xl sm:text-4xl font-semibold text-yellow-500">
-              {state?.title}
+          <div className="flex flex-col sm:flex-row justify-center gap-10 w-full">
+            {/* left section for playing videos and displaying course details to admin */}
+            <div className="space-y-5 sm:w-[28rem] p-4 rounded-lg shadow-[0_0_10px_black]">
+              <video
+                id="video"
+                src={lectures && lectures[currentVideo]?.lecture?.secure_url}
+                className="object-fill rounded-tl-lg rounded-tr-lg w-full"
+                controls
+                disablePictureInPicture
+                muted
+                controlsList="nodownload"
+              ></video>
+              <div>
+                <h1>
+                  <span className="text-yellow-500"> Title: </span>
+                  {lectures && lectures[currentVideo]?.title}
+                </h1>
+                <p>
+                  <span className="text-yellow-500">
+                    Description:{" "}
+                  </span>
+                  {lectures && lectures[currentVideo]?.description}
+                </p>
+              </div>
             </div>
 
-            <div className="flex flex-col sm:flex-row justify-center gap-10 w-full">
-              {/* left section playing videos and displaying course details to admin */}
-              <div className="space-y-5 sm:w-[30rem] p-4 mx-1 rounded-md shadow-[0_0_10px_black]">
-                <video
-                  src={lectures && lectures[currentVideo]?.lecture?.secure_url}
-                  className="object-fill rounded-tl-lg rounded-tr-lg w-full"
-                  controls
-                  disablePictureInPicture
-                  muted
-                  controlsList="nodownload"
-                ></video>
-                <div>
-                  <h1>
-                    <span className="text-yellow-500">Title : </span>
-                    {lectures && lectures[currentVideo]?.title}
-                  </h1>
-                  <p>
-                    <span className="text-yellow-500">Description : </span>
-                    {lectures && lectures[currentVideo]?.description}
-                  </p>
-                </div>
-              </div>
-
-              {/* right section for displaying list of lectures  */}
-              <ul className="sm:w-[30rem] p-4 mx-1 rounded-md shadow-[0_0_10px_black] space-y-4">
-                <li className="font-semibold text-xl text-yellow-500 flex items-center justify-between">
-                  <p>Lectures List</p>
-                  {role === "admin" && (
-                    <button
-                      onClick={() => navigate("/course/addlecture")}
-                      className="btn-primary px-2 py-1 rounded-md font-semibold text-sm"
-                    >
-                      Add new lecture
-                    </button>
-                  )}
-                </li>
-                {lectures &&
-                  lectures.map((lecture, index) => (
-                    <li className="space-y-2" key={lecture._id}>
+            {/* right section for displaying list of lectres */}
+            <ul className="sm:w-[28rem] p-4 rounded-lg shadow-[0_0_10px_black] flex flex-col justify-around">
+              <li className="font-semibold text-xl text-yellow-500 flex items-center justify-between">
+                <p className="border-b border-gray-500 pb-1">Lectures list</p>
+                {role === "admin" && (
+                  <button
+                    onClick={() =>
+                      navigate("/course/addlecture", { state: { ...state } })
+                    }
+                    className="btn-primary px-2 py-1 rounded-md font-semibold text-sm"
+                  >
+                    Add new lecture
+                  </button>
+                )}
+              </li>
+              {lectures &&
+                lectures.map((lecture, idx) => {
+                  return (
+                    <li className="space-y-2 border-b border-gray-500 py-4" key={lecture._id}>
                       <p
                         className="cursor-pointer"
-                        onClick={() => setCurrentVideo(index)}
+                        onClick={() => setCurrentVideo(idx)}
                       >
-                        <span> Lecture {index + 1} : </span>
-                        {lecture.title}
+                        <span> Lecture {idx + 1} : </span>
+                        {lecture?.title} 
+                        
                       </p>
                       {role === "admin" && (
                         <button
-                          onClick={() => handleDelete(state._id, lecture._id)}
-                          className="btn-secondary px-2 py-1 rounded-md font-semibold text-sm"
+                          onClick={() => handleDelete(state?._id, lecture?._id)}
+                          className="btn-accent px-2 py-1 rounded-md font-semibold text-sm"
                         >
-                          Delete Lecture
+                          Delete lecture
                         </button>
                       )}
                     </li>
-                  ))}
-              </ul>
-            </div>
-          </>
+                  );
+                })}
+            </ul>
+          </div>
         ) : (
-          <>
-            <p className="text-2xl font-bold">
-              Sorry, this course doesn&apos;t have any lectures.
-            </p>
+          role === "admin" && (
             <button
-              onClick={() => navigate(-1)}
-              className="btn-secondary flex items-center gap-1 px-4 py-2 rounded-md font-semibold text-base"
+              onClick={() =>
+                navigate("/course/addlecture", { state: { ...state } })
+              }
+              className="btn btn-outline btn-accent px-2 py-1 rounded-md font-semibold text-lg"
             >
-              <AiOutlineArrowLeft/> Go Back
+              Add new lecture
             </button>
-          </>
+          )
         )}
       </div>
     </HomeLayout>
